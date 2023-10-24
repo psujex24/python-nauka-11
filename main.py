1e5c277094d0b4e69e6bce118c7292372903ed81
Jakub Psujek <kubakubakuba113@gmail.com>
2023-10-24 18:12:18 +0200

    koniec

---
+++ main.py
@@ -0,0 +1,18 @@
+import random
+
+print(random.randint(1, 10))
+
+from random import randint
+
+print(randint(1, 10))
+
+from math import pi
+
+print(pi)
+
+from math import sqrt as pierwiastek
+
+print(pierwiastek(9))
+print(int(pierwiastek(16)))
+
+#moduły docs.python.org/3/py-modindex.html
